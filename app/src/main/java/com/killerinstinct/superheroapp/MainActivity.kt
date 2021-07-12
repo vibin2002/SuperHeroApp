ea043e443b4977dedc23e47da1cabd1857701f7a
Vibin <vibin.cr22@gmail.com>
2021-07-12 20:34:41 +0530

    Color Palette from androidx Pallete applied!

--- app/src/main/java/com/killerinstinct/superheroapp/MainActivity.kt
+++ app/src/main/java/com/killerinstinct/superheroapp/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.killerinstinct.superheroapp
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.transition.Fade
 import android.util.Log
 import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import androidx.lifecycle.Observer
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.killerinstinct.superheroapp.adapters.HeroAdapter
 import com.killerinstinct.superheroapp.data.HeroResponse
 import com.killerinstinct.superheroapp.databinding.ActivityMainBinding
@@ -19,7 +22,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(),HeroAdapter.OnHeroItemClickListener {
+class MainActivity : AppCompatActivity(), HeroAdapter.OnHeroItemClickListener {
 
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
@@ -28,30 +31,23 @@ class MainActivity : AppCompatActivity(),HeroAdapter.OnHeroItemClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        window.statusBarColor = Color.BLACK
 
         var list = listOf<HeroResponse>()
 
 
-//        lifecycleScope.launchWhenStarted {
-//            list = viewModel._herolist.value!!
-//            Log.d(
-//                "DoctorStrange",
-//                list.toString()
-//            )
-//        }
 
+
+        window.exitTransition = Fade()
         viewModel._herolist.observe(this, Observer {
             list = it
             Log.d("DoctorStrange", list.toString())
             list.forEach {
-                println("booyah ${it.id} ${it.name}" )
+                println("booyah ${it.id} ${it.name}")
             }
-            if(list.isEmpty())
-            {
+            if (list.isEmpty()) {
                 findViewById<ProgressBar>(R.id.progress_bar).visibility = ProgressBar.VISIBLE
-            }
-            else
-            {
+            } else {
 
                 findViewById<ProgressBar>(R.id.progress_bar).visibility = ProgressBar.INVISIBLE
                 setupRecyclerView(list)
@@ -60,21 +56,30 @@ class MainActivity : AppCompatActivity(),HeroAdapter.OnHeroItemClickListener {
         setupRecyclerView(list)
     }
 
-    private fun setupRecyclerView(list: List<HeroResponse>)
-    {
-        binding.heroRecyclerView.layoutManager = GridLayoutManager(this,2)
-        binding.heroRecyclerView.adapter = HeroAdapter(list,this)
+    private fun setupRecyclerView(list: List<HeroResponse>) {
+        binding.heroRecyclerView.layoutManager = GridLayoutManager(this, 2)
+        binding.heroRecyclerView.adapter = HeroAdapter(list, this)
     }
 
-    override fun onClick(position: Int, heroResponse: HeroResponse,binding: HeroCardBinding) {
+    override fun onClick(position: Int, heroResponse: HeroResponse, binding: HeroCardBinding) {
+        val bmp = (binding.heroImage.drawable as BitmapDrawable).bitmap.copy(
+            Bitmap.Config.ARGB_8888,
+            true
+        )
+        val x: Int? = Palette.from(bmp).generate().run {
+            dominantSwatch?.rgb
+        } ?: Color.BLACK
+
         val activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
             this,
-                binding.heroImage,
+            binding.heroImage,
             "imageTrans"
         )
+        supportStartPostponedEnterTransition()
         Intent(this, StatsActivity::class.java).apply {
             putExtra("Hero", heroResponse)
-            startActivity(this,activityOptionsCompat.toBundle())
+            putExtra("pallete",x)
+            startActivity(this, activityOptionsCompat.toBundle())
         }
     }
 
--- app/src/main/java/com/killerinstinct/superheroapp/StatsActivity.kt
+++ app/src/main/java/com/killerinstinct/superheroapp/StatsActivity.kt
@@ -1,10 +1,14 @@
 package com.killerinstinct.superheroapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.transition.Fade
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.killerinstinct.superheroapp.data.HeroResponse
 import com.killerinstinct.superheroapp.databinding.ActivityStatsBinding
+
 
 class StatsActivity : AppCompatActivity() {
 
@@ -16,13 +20,21 @@ class StatsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val hero = intent.getSerializableExtra("Hero") as HeroResponse
+        val palette = intent.getIntExtra("pallete",Color.RED)
+        val gd = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(palette, -0xececed)
+        )
+
+        window.statusBarColor = palette
+        gd.cornerRadius = 0f
+
 
         Glide.with(this).load(hero.images!!.sm).into(binding.saHeroImage)
 
-//        binding.progressBar.apply {
-//            progress = hero.powerstats?.intelligence!!
-//        }
+        binding.consLayout.background = gd
 
 
+
+        window.enterTransition = Fade()
     }
 }
